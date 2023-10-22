2d78f1ec059dbc400aac56c1d4fe3bdc04cf6a6f
alexey kozlov <a.o.kozlov@vk.team>
2023-10-22 18:24:27 +0300

    4.16. Больше деталей

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,6 @@
-/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
 const checkStringLength = (inputString, maxLength) =>
   inputString.length <= maxLength;
-
-console.log(checkStringLength('проверяемая строка', 20));
-console.log(checkStringLength('проверяемая строка', 18));
-console.log(checkStringLength('проверяемая строка', 10));
 
 // строим строку в обратной последовательности
 const isPalindrom = (inputString) => {
@@ -32,14 +28,6 @@ const isPalindrom2 = (inputString) => {
   return true;
 };
 
-console.log('топот',isPalindrom('топот')); // true
-// Несмотря на разный регистр, тоже палиндром
-console.log('ДовОд',isPalindrom('ДовОд')); // true
-// Это не палиндром
-console.log('Кекс',isPalindrom('Кекс')); // false
-// Это  палиндром
-console.log('Лёша на полке клопа нашёл ', isPalindrom2('Лёша на полке клопа нашёл ')); // true
-
 const extractNumbers = (inputString) => {
   const stringedInput = String(inputString);
   let res = '';
@@ -52,12 +40,3 @@ const extractNumbers = (inputString) => {
   return parseInt(res, 10);
 };
 
-console.log('2023 год',extractNumbers('2023 год')); // 2023
-console.log('ECMAScript 2022', extractNumbers('ECMAScript 2022')); // 2022
-console.log('1 кефир, 0.5 батона', extractNumbers('1 кефир, 0.5 батона')); // 105
-console.log('агент 007', extractNumbers('агент 007')); // 7
-console.log('а я томат', extractNumbers('а я томат')); // NaN
-
-console.log(2023, extractNumbers(2023)); // 2023
-console.log(-1, extractNumbers(-1)); // 1
-console.log(1.5, extractNumbers(1.5)); // 15
