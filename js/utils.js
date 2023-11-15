0b2f18f1902abd5a83acac2744586a05caa0c4a8
alexey kozlov <a.o.kozlov@vk.team>
2023-11-15 17:57:02 +0300

    12.11. Получить функции от Кекса

--- js/utils.js
+++ js/utils.js
@@ -34,3 +34,34 @@ export const createIdGenerator = () => {
 };
 
 export const isEscape = (evt) => evt.key === 'Escape';
+
+export function debounce(func, ms) {
+  let timeout = null;
+  return function() {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(() => func(...arguments), ms);
+  };
+}
+
+function throttle (callback, delayBetweenFrames) {
+  // Используем замыкания, чтобы время "последнего кадра" навсегда приклеилось
+  // к возвращаемой функции с условием, тогда мы его сможем перезаписывать
+  let lastTime = 0;
+
+  return (...rest) => {
+    // Получаем текущую дату в миллисекундах,
+    // чтобы можно было в дальнейшем
+    // вычислять разницу между кадрами
+    const now = new Date();
+
+    // Если время между кадрами больше задержки,
+    // вызываем наш колбэк и перезаписываем lastTime
+    // временем "последнего кадра"
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+}
